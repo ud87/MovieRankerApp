23c7f118665c57cbd8f35f7c359cb39ef65c80b4
Udaya <udayarai@hotmail.co.uk>
2025-02-19 22:09:17 +0000

    MoviesModel added

---
+++ Movie Ranker App/Movie Ranker/Models/MoviesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations; //used for data annotations like key, required etc
+
+namespace Movie_Ranker.Models
+{
+    public class MoviesModel
+    {
+        [Key]   //need to set id as primary key else EF will not be able to track and manage the entity
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Studio {  get; set; }
+        public int Score { get; set; }
+    }
+}
