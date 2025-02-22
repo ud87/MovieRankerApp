9cc40418e6d1679e0f98b3c3ee73586dbccf0309
Udaya <udayarai@hotmail.co.uk>
2025-02-22 15:47:50 +0000

    Create, Edit method added to MovieController.cs, Create.cshtml and Edit.cshtml added to Views\Movie

--- Movie Ranker App/Movie Ranker/Controllers/MovieController.cs
+++ Movie Ranker App/Movie Ranker/Controllers/MovieController.cs
@@ -22,5 +22,58 @@ namespace Movie_Ranker.Controllers
             IEnumerable<MovieModel> objMovieList = _db.Movies.ToList(); //retrieves all the movies from the database and stores them in objMovieList
             return View(objMovieList); //returns the view with the list of movies
         }
+
+        //get method to create a new movie
+        public IActionResult Create()
+        {
+            return View(); //there is no need to retrieve any data from the db as it is simply displaying an empty form for the user to fill out so its blank
+        }
+
+        //post method to create a new movie
+        [HttpPost]
+        [ValidateAntiForgeryToken] // used to validate anti forgery
+        public IActionResult Create(MovieModel movie)
+        {
+            if (ModelState.IsValid) //checks if the model state is valid
+            {
+                _db.Movies.Add(movie); //adds the movie to the database
+                _db.SaveChanges(); //saves the changes to the database
+                return RedirectToAction("Index"); //redirects to the Index action
+            }
+            return View(movie); //returns the view with the movie model
+        }
+
+
+        //get method to edit existing movie
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var movie = _db.Movies.Find(id); //finds the movie with the id passed in the parameter
+                if (movie == null)
+                {
+                    return NotFound();
+                }
+                return View(movie);
+            }          
+        }
+
+        //post method to edit existing movie
+        [HttpPost]
+        [ValidateAntiForgeryToken] // used to validate anti forgery
+        public IActionResult Edit(MovieModel movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Movies.Update(movie); //updates the movie in the database
+                _db.SaveChanges(); //saves the changes to the database
+                return RedirectToAction("Index");  //redirects to the Index action
+            }
+            return View(movie); //returns the view with the movie model
+        }
     }
 }
