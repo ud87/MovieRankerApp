e043518c5adbc462973a5af60f0bedebd5ae943b
Udaya <udayarai@hotmail.co.uk>
2025-02-23 13:49:30 +0000

    Front landing page added to Home/Index.cshtml

--- Movie Ranker App/Movie Ranker/Models/MovieModel.cs
+++ Movie Ranker App/Movie Ranker/Models/MovieModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations; //used for data annotations like key, required etc 
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations; //used for data annotations like key, required etc 
 
 namespace Movie_Ranker.Models
 {
@@ -6,12 +7,23 @@ namespace Movie_Ranker.Models
     {
         [Key]   //need to set id as primary key else EF will not be able to track and manage the entity
         public int Id { get; set; }
-        public string MovieName { get; set; }
-        public string Genre { get; set; }
+        [Display(Name = "Movie Name")]           //allows us to display Movie Name instead of MovieName
+
+        [Required]
+        public string? MovieName { get; set; }
+
+        [Required]
+        public string? Genre { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode =true)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        public string Studio {  get; set; }
+
+        [Required]
+        public string? Studio {  get; set; }
+
+        [Range(0, 100, ErrorMessage ="Value must be between 0 and 100")]
         public int Score { get; set; }
     }
 }
