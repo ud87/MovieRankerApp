67997678f99ffd64a8787e4519616851a6895e21
Udaya <udayarai@hotmail.co.uk>
2025-03-14 20:15:50 +0000

    LoginViewModel added

---
+++ MovieRankerApp/Movie Ranker App/Movie Ranker/Migrations/20250314201023_LoginViewModel_Added.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movie_Ranker.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoginViewModel_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
