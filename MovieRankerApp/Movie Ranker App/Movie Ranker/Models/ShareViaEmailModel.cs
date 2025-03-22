cb6f394ac5a7cf292bcd1bcd8475e58f6db1cfe9
Udaya <udayarai@hotmail.co.uk>
2025-03-22 11:48:01 +0000

    ShareViaEmailModel.cs added to provide email to send list to

---
+++ MovieRankerApp/Movie Ranker App/Movie Ranker/Models/ShareViaEmailModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Ranker.Models
+{
+    public class ShareViaEmailModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? EmailToSend { get; set; } //this is the email address to send the movie list to, will not be stored in database
+    }
+}
