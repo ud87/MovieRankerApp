331bf7ccf6ba8c35145fc74c7a1b34ec0c3ea8ee
Udaya <udayarai@hotmail.co.uk>
2025-03-22 20:38:23 +0000

    HealthController.cs added to run cronjob to keep server alive

---
+++ MovieRankerApp/Movie Ranker App/Movie Ranker/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Ranker.Controllers
+{
+    [ApiController]     //sets the controller to be an API controller
+    [Route("api/[controller]")]   //sets the route to /api/health
+    public class HealthController : ControllerBase //inherits from ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Server is alive!");
+        }
+    }
+}
