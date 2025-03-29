ee3f4a96e2b259fad8917c11ee69e2c663a13596
Udaya <udayarai@hotmail.co.uk>
2025-03-29 12:15:03 +0000

    NewPassword, ConfirmPasword, Token added

---
+++ MovieRankerApp/Movie Ranker App/Movie Ranker/Models/ResetPasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Ranker.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? NewPassword { get; set; } //new password for the account
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+    }
+}
