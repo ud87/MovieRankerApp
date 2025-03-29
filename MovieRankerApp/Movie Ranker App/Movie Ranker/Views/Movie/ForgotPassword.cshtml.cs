d240cc9a1e1df96b53042582b9ca859877fd67f9
Udaya <udayarai@hotmail.co.uk>
2025-03-29 12:17:35 +0000

    ForgotPassword.cshtml cancel button changed to go to login instead of index

---
+++ MovieRankerApp/Movie Ranker App/Movie Ranker/Views/Movie/ForgotPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movie_Ranker.Views.Movie
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
